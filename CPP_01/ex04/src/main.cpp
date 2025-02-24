62266b0c6747a52ac8656ac98c811690907652ee
TheBetterDark <91607604+TheBetterDark@users.noreply.github.com>
2025-02-24 14:47:58 +0000

    Add initial implementation of sed program with Makefile and test script

---
+++ CPP_01/ex04/src/main.cpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 13:34:40 by muabdi            #+#    #+#             */
+/*   Updated: 2025/02/24 14:25:18 by muabdi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void print_usage(const char *programName)
+{
+    std::cout << "Usage: " << programName << " <filename> <string_to_replace> <replacement_string>" << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+bool read_file(const std::string &filename, std::string &fileContents)
+{
+    std::ifstream fileIn(filename.c_str());
+    if (!fileIn.is_open())
+    {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return false;
+    }
+
+    std::string line;
+    while (std::getline(fileIn, line))
+    {
+        fileContents += line + "\n";
+    }
+    fileIn.close();
+    return true;
+}
+
+void replace_string(std::string &fileContents, const std::string &stringToReplace, const std::string &replacementString)
+{
+    size_t pos = 0;
+    while ((pos = fileContents.find(stringToReplace, pos)) != std::string::npos)
+    {
+        fileContents.replace(pos, stringToReplace.length(), replacementString);
+        pos += replacementString.length();
+    }
+}
+
+bool write_file(const std::string &filename, const std::string &fileContents)
+{
+    std::ofstream fileOut(filename.c_str());
+    if (!fileOut.is_open())
+    {
+        std::cerr << "Error: Could not create file " << filename << std::endl;
+        return false;
+    }
+
+    fileOut << fileContents;
+    fileOut.close();
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+        print_usage(argv[0]);
+
+    std::string filename = argv[1];
+    std::string stringToReplace = argv[2];
+    std::string replacementString = argv[3];
+    std::string fileContents;
+
+    if (!read_file(filename, fileContents))
+        return EXIT_FAILURE;
+
+    replace_string(fileContents, stringToReplace, replacementString);
+
+    if (!write_file(filename + ".replace", fileContents))
+        return EXIT_FAILURE;
+
+    std::cout << "File " << filename << " has been successfully replaced and saved as " << filename + ".replace" << std::endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ CPP_01/ex04/tests.sh
@@ -0,0 +1,68 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tests.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/02/24 14:25:29 by muabdi            #+#    #+#              #
+#    Updated: 2025/02/24 14:47:22 by muabdi           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+# Clear outp
+clear
+
+# Compile the main.cpp file
+make re
+if [ $? -ne 0 ]; then
+    echo "Compilation failed"
+    exit 1
+fi
+
+echo "\n"
+
+# Create a temporary input file
+input_file="test_input.txt"
+echo "Hello World\nThis is a test file\nHello World" > $input_file
+
+# Run the program with test inputs
+./sed $input_file "Hello" "Hi"
+if [ $? -ne 0 ]; then
+    echo "Program execution failed"
+    exit 1
+fi
+
+# Check if the output file is created
+output_file="${input_file}.replace"
+if [ ! -f $output_file ]; then
+    echo "Output file not created"
+    rm $input_file
+    exit 1
+fi
+
+# Verify the contents of the output file
+expected_output="expected_output.txt"
+echo "Hi World\nThis is a test file\nHi World" > $expected_output
+
+if ! diff -q $output_file $expected_output > /dev/null; then
+    echo "\nOutput file contents do not match expected output\n"
+    echo "Expected:"
+    cat $expected_output
+    echo "Got:"
+    cat $output_file
+    rm $input_file $output_file $expected_output sed
+    exit 1
+fi
+
+echo "\n"
+cat $expected_output
+echo "\n"
+cat $output_file
+
+# Clean up
+rm -rf $input_file $output_file $expected_output sed bin
+
+echo "\nAll tests passed!\n"
